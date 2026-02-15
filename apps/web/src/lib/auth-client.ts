85202b77e88a89c325f95719df6c15105f001310
Linsmar <linsmarvital@chuva-inc.com>
2026-02-15 20:04:36 -0300

    chore: remove todo o barebones do projeto para recomeçar com react router

--- apps/web/src/lib/auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "@ranked-lists/env/web";
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-  baseURL: env.VITE_SERVER_URL,
-});
